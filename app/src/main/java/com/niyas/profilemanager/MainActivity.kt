1752aaf66df7bf63af2822c683ef81e202e76576
niyasahamed01 <niyasahamed.mofficial@gmail.com>
2024-10-09 17:08:17 +0530

    optimize code

--- app/src/main/java/com/niyas/profilemanager/MainActivity.kt
+++ app/src/main/java/com/niyas/profilemanager/MainActivity.kt
@@ -1,20 +1,8 @@
 package com.niyas.profilemanager
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.NavController
-import androidx.navigation.Navigation.findNavController
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import com.niyas.profilemanager.adapter.ProfileAdapter
-import com.niyas.profilemanager.databinding.ActivityMainBinding
-import com.niyas.profilemanager.roomdb.Profile
-import com.niyas.profilemanager.viewmodel.ProfileViewModel
+
 
 class MainActivity : AppCompatActivity() {
 
